9a06e3c9800eaae9c3c443339c4b6ea2263ece3a
Wu <astrowu61@gmail.com>
2024-10-21 00:08:34 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import "./style.css";
+const dino = document.querySelector("#dino");
+const block = document.querySelector("#block");
+window.addEventListner("keydown", (event) => {
+  console.log(event);
+  if (event.code == "Space") {
+    console.log("Jump fucking High");
+    dino.classList.add("jumpClass");
+    setTimeout(() => {
+      dino.classList.remove("jumpClass");
+    }, 300);
+  }
+});
+
+setInterval(() => {
+  const dinoBottom = parseFloat(
+    getComputedStyle(dino).getPropertyValue("bottom")
+  );
+  const blockLeft = parseFloat(
+    getComputedStyle(block).getPropertyValue("left")
+  );
+  if (blockLeft < 20 && blockLeft > -20 && dinoBottom <= 20) {
+    console.log("Bloody Death");
+  }
+}, 10);
+
